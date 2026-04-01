65d55e048e8e4c5712fad2f9216d698d6c34c4c9
Sammy Taubman <staubman1@gmail.com>
2026-04-01 18:11:33 -0400

    template

---
+++ vite.config.js
@@ -0,0 +1,36 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), VitePWA({
+    registerType: 'autoUpdate',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: '\u0017\u0017Qat Cafe',
+      short_name: 'qat-cafe',
+      description: 'A lightweight PWA for playing word games with friends',
+      theme_color: '#D4F4DD',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+})
